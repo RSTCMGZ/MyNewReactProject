fed1582920e6ef68b2bfb6714ba7aac7d1b50aeb
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2023-10-17 14:55:02 +0300

    delete productData

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,11 +1,10 @@
 import ProductItem from "./ProductItem";
-import { productData } from "../../productData";
 import "./Products.css";
 import NewProduct from "../NewProduct/NewProduct";
 import { useState } from "react";
 
 const Products = () => {
-  const [products, setProducts] = useState(productData);
+  const [products, setProducts] = useState([]);
 
   return (
     <div className="product-wrapper">
@@ -13,10 +12,16 @@ const Products = () => {
       <h1 className="text-center mb-5 text-danger fw-bold text-uppercase">
         Products
       </h1>
-      <div className="products">
-        {products.map((product) => (
-          <ProductItem key={product.productName} product={product} />
-        ))}
+      <div className="products text-center flex-wrap d-flex align-items-center justify-content-center  gap-5">
+        {products.length === 0 ? (
+          <h2 className="text-center text-danger fw-bold text-uppercase">
+            Sepette ürün yok!
+          </h2>
+        ) : (
+          products.map((product) => (
+            <ProductItem key={product.productName} product={product} />
+          ))
+        )}
       </div>
     </div>
   );
